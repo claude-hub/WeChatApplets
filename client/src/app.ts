f32b48fa206ef74ba00cec920c3486492a102772
claude-hub <314705487@qq.com>
2021-03-05 21:50:34 +0800

    chore: use hooks

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,26 +1,13 @@
-import React, { Component } from 'react'
-import Taro, { Config } from '@tarojs/taro'
+import { useEffect } from 'react'
+import Taro from '@tarojs/taro'
 
-import './app.less'
-
-class App extends Component {
-
-  componentDidMount () {
+function App(props) {
+  useEffect(() => {
     if (process.env.TARO_ENV === 'weapp') {
       Taro.cloud.init()
     }
-  }
-
-  componentDidShow () {}
-
-  componentDidHide () {}
-
-  componentDidCatchError () {}
-
-  // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
-  }
+  }, [])
+  return props.children
 }
 
 export default App
--- client/src/components/login/index.weapp.tsx
+++ client/src/components/login/index.weapp.tsx
@@ -1,41 +1,28 @@
-import React, { Component } from 'react'
+import { useState } from 'react'
 import Taro from '@tarojs/taro'
 import { View, Text, Button } from '@tarojs/components'
 
-export default class Index extends Component {
-  state = {
-    context: {}
-  }
 
-  componentWillMount() {}
+function Login() {
+  const [str, setStr] = useState("");
 
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  getLogin = () => {
+  function getLogin() {
     Taro.cloud
       .callFunction({
         name: "login",
         data: {}
       })
       .then(res => {
-        this.setState({
-          context: res.result
-        })
+        setStr(JSON.stringify(res.result))
       })
   }
 
-  render() {
-    return (
-      <View className='index'>
-        <Button onClick={this.getLogin}>获取登录云函数</Button>
-        <Text>context：{JSON.stringify(this.state.context)}</Text>
-      </View>
-    )
-  }
+  return (
+    <View>
+      <Button onClick={getLogin}>获取登录云函数</Button>
+      <Text>{str}</Text>
+    </View>
+  )
 }
+
+export default Login;
